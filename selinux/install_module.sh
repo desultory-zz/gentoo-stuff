ce5c1c3d11df9210d81abc6d40e69576de053263
desu <11171762+desultory@users.noreply.github.com>
2021-08-04 01:13:37 -0500

    modified:   install_module.sh

--- selinux/install_module.sh
+++ selinux/install_module.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-tr -d '\r' < $1.te > $1-converted.te
+tr -d '\r' < $1.te > $1-converted
 
-checkmodule -M -m -o $1.mod $1-converted.te
+checkmodule -M -m -o $1.mod $1-converted
 rm $1-converted
 semodule_package -m $1.mod -o $1.pp
 semodule -i $1.pp
