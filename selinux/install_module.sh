287e2d4f122193d74a36241127d94c9a704a1f72
desu <11171762+desultory@users.noreply.github.com>
2021-08-11 02:27:01 -0500

    modified:   install_module.sh

--- selinux/install_module.sh
+++ selinux/install_module.sh
@@ -1,8 +1,49 @@
 #!/bin/bash
 
-tr -d '\r' < $1.te > $1-converted
+if [[ -z $1 ]]; then
+	echo "You must specify a module name"
+	exit 1
+fi
 
-checkmodule -M -m -o $1.mod $1-converted
-rm $1-converted
-semodule_package -m $1.mod -o $1.pp
-semodule -i $1.pp
+if [[ "$1" == "-h" ]]; then
+	echo "Arguement should be module name"
+	exit 1
+fi
+
+name=$1
+
+if [[ -f $name ]]; then
+	name=$(echo "$name" | cut -f 1 -d '.')
+fi
+
+if [[ -f "$name.te" ]]; then
+	tr -d '\r' < $name.te > $name-converted
+else
+	echo "$name.te not found"
+	exit 1
+fi
+
+checkmodule -M -m -o $name.mod $name-converted
+ret=$?
+rm $name-converted
+if [[ $ret -eq 0 ]]; then
+	semodule_package -m $name.mod -o $name.pp
+	ret=$?
+	rm $name.mod
+	if [[ $ret -eq 0 ]]; then
+		semodule -i $name.pp
+		 ret=$?
+		rm $name.pp
+		if [[ $ret -ne 0 ]]; then
+			echo "Failed to install package"
+			exit 1
+		fi
+	else
+		echo "Failed to package module"
+		exit 1
+	fi
+else
+	echo "Failed to create module"
+	exit 1
+fi
+
