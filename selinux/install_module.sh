163ac5e3169f9f571c5659c7e6691414c49236a9
desu <11171762+desultory@users.noreply.github.com>
2021-08-11 02:40:33 -0500

    modified:   install_module.sh

--- selinux/install_module.sh
+++ selinux/install_module.sh
@@ -11,40 +11,35 @@ if [[ "$1" == "-h" ]]; then
 fi
 
 name=$1
-rand=$RANDOM
-randname=$name-$rand
 
 if [[ -f $name ]]; then
-	tr -d '\r' < $name > $randname
 	name=$(echo "$name" | cut -f 1 -d '.')
 elif [[ -f "$name.te" ]]; then
-	tr -d '\r' < $name.te > $randname
+	checkmodule -M -m -o $name.mod $name.te
+	ret=$?
+	if [[ $ret -eq 0 ]]; then
+		semodule_package -m $name.mod -o $name.pp
+		ret=$?
+		rm $name.mod
+		if [[ $ret -eq 0 ]]; then
+			semodule -i $name.pp
+			ret=$?
+			rm $name.pp
+			if [[ $ret -ne 0 ]]; then
+				echo "Failed to install package"
+				exit 1
+			fi
+		else
+			echo "Failed to package module"
+			exit 1
+		fi
+	else
+		echo "Failed to create module"
+		exit 1
+	fi
 else
 	echo "$name.te not found"
 	exit 1
 fi
 
-checkmodule -M -m -o $randname.mod $randname
-ret=$?
-rm $randname
-if [[ $ret -eq 0 ]]; then
-	semodule_package -m $randname.mod -o $randname.pp
-	ret=$?
-	rm $randname.mod
-	if [[ $ret -eq 0 ]]; then
-		semodule -i $randname.pp
-		ret=$?
-		rm $randname.pp
-		if [[ $ret -ne 0 ]]; then
-			echo "Failed to install package"
-			exit 1
-		fi
-	else
-		echo "Failed to package module"
-		exit 1
-	fi
-else
-	echo "Failed to create module"
-	exit 1
-fi
 
